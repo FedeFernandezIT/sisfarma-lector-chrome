90d361043687bebd023cbfa18c6fb64a77dcd556
Federico Fernández <fede.fernandez.it@gmail.com>
2019-12-14 15:06:38 -0300

    installer

--- Lector.Sharp.Wpf/App.xaml.cs
+++ Lector.Sharp.Wpf/App.xaml.cs
@@ -16,47 +16,47 @@ namespace Lector.Sharp.Wpf
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {            
-            if (!IsRunAsAdministrator())
-            {                
-                var location = Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.Programs),
-                    @"SisFarma", @"SisFarma Lector.appref-ms");
+        //protected override void OnStartup(StartupEventArgs e)
+        //{            
+        //    if (!IsRunAsAdministrator())
+        //    {                
+        //        var location = Path.Combine(
+        //            Environment.GetFolderPath(Environment.SpecialFolder.Programs),
+        //            @"SisFarma", @"SisFarma Lector.appref-ms");
 
                 
-                // Configurar Lector como Administrador                
-                ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", $"/c \"{location}\""); // same as "netsh interface ip delete arpcache"
-                processInfo.CreateNoWindow = true;
-                processInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                processInfo.UseShellExecute = true;
-                processInfo.Verb = "runas";                
+        //        // Configurar Lector como Administrador                
+        //        ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", $"/c \"{location}\""); // same as "netsh interface ip delete arpcache"
+        //        processInfo.CreateNoWindow = true;
+        //        processInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        //        processInfo.UseShellExecute = true;
+        //        processInfo.Verb = "runas";                
                 
-                // Ejecutar Lector
-                try
-                {
-                    var cmd = Process.Start(processInfo);                    
-                }
-                catch (Exception ex)
-                {                    
-                    // El usuario no permite que Lector se ejecute como Administrador
-                    MessageBox.Show("Sisfarma Lector, sólo puede ejecutarse como Administrador.");
-                }
-                // Cerrar Lector (el primero que se ejecuta sin permisos)                
-                Application.Current.Shutdown();
-            }
+        //        // Ejecutar Lector
+        //        try
+        //        {
+        //            var cmd = Process.Start(processInfo);                    
+        //        }
+        //        catch (Exception ex)
+        //        {                    
+        //            // El usuario no permite que Lector se ejecute como Administrador
+        //            MessageBox.Show("Sisfarma Lector, sólo puede ejecutarse como Administrador.");
+        //        }
+        //        // Cerrar Lector (el primero que se ejecuta sin permisos)                
+        //        Application.Current.Shutdown();
+        //    }
 
-            if (IsRunAsAdministrator() && !Current.SetSingleInstance())
-            {
-                MessageBox.Show("Application is already running!",
-                                "ALREADY ACTIVE",
-                                MessageBoxButton.OK,
-                                MessageBoxImage.Exclamation);
-                Current.Shutdown(-1);
-            }
+        //    if (IsRunAsAdministrator() && !Current.SetSingleInstance())
+        //    {
+        //        MessageBox.Show("Application is already running!",
+        //                        "ALREADY ACTIVE",
+        //                        MessageBoxButton.OK,
+        //                        MessageBoxImage.Exclamation);
+        //        Current.Shutdown(-1);
+        //    }
             
-            base.OnStartup(e);
-        }
+        //    base.OnStartup(e);
+        //}
 
         private bool IsRunAsAdministrator()
         {
